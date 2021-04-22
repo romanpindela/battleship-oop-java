195b1979a5ece206c24cfccb7d5924110e9c0cad
Eseriol <eseriol@outlook.com>
2021-04-22 12:02:49 +0200

    quick switch for menu options, rest on the way by midnight - one file didnt changed

--- src/main/java/game/Battleship/Main.java
+++ src/main/java/game/Battleship/Main.java
@@ -8,13 +8,6 @@ import game.Input.Input;
 public class Main {
     public static void main(String[] args) {
         Display display = new Display();
-        //display.gameMenu();
-
-        Board board = new Board();
-        DisplayBoard displayBoard = new DisplayBoard();
-        displayBoard.displayBoard(board);
-
-        int[] newCoords = Input.changeCoordsFormat("A1");
-        System.out.println(newCoords[0] + "" + newCoords[1]);
+        display.gameMenu();
     }
 }
