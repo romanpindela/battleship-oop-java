3f365d9dc46d232ff6a06a102a04b002a99c3a87
FabolousBishop <bartosz.tomaszewski.1994@gmail.com>
2021-04-23 22:45:48 +0200

    Update FightPhase.java
    Added checkIfShipDestroyed method which checks all the squares and changes their status to DESTROYED if they're all HIT

--- src/main/java/game/Battleship/FightPhase.java
+++ src/main/java/game/Battleship/FightPhase.java
@@ -1,5 +1,13 @@
 package game.Battleship;
 
+import game.Board.Square;
+import game.Board.SquareStatus;
+import game.Ship.Ship;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FightPhase extends Game{
     public boolean lost;
 
@@ -15,6 +23,38 @@ public class FightPhase extends Game{
         return this.lost;
     }
 
+    public static void checkIfShipIsDestroyed (Ship targetedShip){
 
+        List<Square> hitSquares =
+                targetedShip.squares.stream()
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList())
+                        .stream()
+                        .filter(square -> square.status == SquareStatus.squareStatus.HIT)
+                        .collect(Collectors.toList());
 
+        if (hitSquares.size() == targetedShip.squares.size()) {
+            for (int i = 0; i< targetedShip.squares.size(); i++){
+                targetedShip.squares.get(i).setStatus(SquareStatus.squareStatus.DESTROYED);
+            }
+
+        }
+
+    }
+
+    /*
+
+    public List<Square> findEmptySquares(ArrayList<ArrayList<Square>> boardOfSquares) {
+        List<Square> emptySquares =
+                boardOfSquares.stream()
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList())
+                        .stream()
+                        .filter(square -> square.status == SquareStatus.squareStatus.EMPTY)
+                        .collect(Collectors.toList());
+
+        return emptySquares;
+    }
+
+     */
 }
